4ed1632e29abf575dbcd7ccd3c3688d1485069b2
jzalacain <jzalacain@avalon>
2018-04-23 17:42:28 -0400

    remove in out package

--- src/main/java/org/logicware/storage/StoragePoolExecutor.java
+++ src/main/java/org/logicware/storage/StoragePoolExecutor.java
@@ -26,7 +26,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.logicware.Predicate;
 import org.logicware.Storage;
