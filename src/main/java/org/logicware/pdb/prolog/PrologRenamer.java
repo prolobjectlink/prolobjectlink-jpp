ad1226c4dbcc19604ceff2ba9cee9bda5e299a3c
jzalacain <jzalacain@avalon>
2018-06-21 18:37:55 -0400

    : Equals attribute name
    Task-Url:

---
+++ src/main/java/org/logicware/pdb/prolog/PrologRenamer.java
@@ -0,0 +1,103 @@
+package org.logicware.pdb.prolog;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.logicware.pdb.util.JavaAsserts;
+import org.logicware.pdb.util.JavaReflect;
+import org.logicware.pdb.util.JavaStrings;
+
+/**
+ * Attribute renamer to prevent variable clashes in prolog queries. Basically
+ * convert a class field name in one qualified prolog variable name. The
+ * qualified prolog variable name respect the prolog variable syntax. Have
+ * methods for convert from {@link Field} to {@link PrologVariable} and
+ * vice-versa. E.g
+ * 
+ * 
+ * from {@code org.logicware.domain.geometry.Point.idp} to
+ * {@code ORG_LOGICWARE_DOMAIN_GEOMETRY_POINT_idp_0} from
+ * {@code ORG_LOGICWARE_DOMAIN_GEOMETRY_POINT_idp_0 } to
+ * {@code org.logicware.domain.geometry.Point.idp}
+ * 
+ * 
+ * @author Jose Zalacain
+ * @see #toField(PrologVariable)
+ * @see #toVariable(Field)
+ * @since 1.0
+ */
+final class PrologRenamer {
+
+	// provider for variable creation
+	private final PrologProvider provider;
+
+	// map prolog renamed variable name to declared class
+	private final Map<String, Class<?>> varMap;
+
+	PrologRenamer(PrologProvider provider) {
+		varMap = new HashMap<String, Class<?>>();
+		this.provider = provider;
+	}
+
+	/**
+	 * Convert from {@link Field} to {@link PrologVariable} e.g from
+	 * {@code org.logicware.domain.geometry.Point.idp} to
+	 * {@code ORG_LOGICWARE_DOMAIN_GEOMETRY_POINT_idp_0}
+	 * 
+	 * @param field to be converted
+	 * @return prolog variable renamed that represent this field
+	 * @since 1.0
+	 */
+	public final PrologVariable toVariable(Field field) {
+		Field workField = JavaAsserts.requireNotNull(field);
+		Class<?> workClass = workField.getDeclaringClass();
+		String className = workClass.getName();
+		String name = className.replace('.', '_');
+		name = JavaStrings.toUpperCase(name);
+		name += "_" + workField.getName();
+		int index = -1;
+		Field[] fields = workClass.getDeclaredFields();
+		for (int i = 0; i < fields.length; i++) {
+			if (field.equals(fields[i])) {
+				index = i;
+			}
+		}
+		name += "_" + index;
+		varMap.put(name, workClass);
+		return provider.newVariable(name, index);
+	}
+
+	/**
+	 * Convert from {@link PrologVariable} to {@link Field} e.g from
+	 * {@code ORG_LOGICWARE_DOMAIN_GEOMETRY_POINT_idp_0 } to
+	 * {@code org.logicware.domain.geometry.Point.idp}
+	 * 
+	 * 
+	 * @param variable prolog variable to be converted
+	 * @return equivalent filed to prolog variable.
+	 * @since 1.0
+	 */
+	public final Field toField(PrologVariable variable) {
+		return toField(variable.getName());
+	}
+
+	public final Field toField(String name) {
+		String workName = JavaAsserts.requireNotNull(name);
+		Class<?> workClass = varMap.get(workName);
+		String message = name + "don't belong to any class field";
+		workClass = JavaAsserts.notNull(workClass, message);
+		String className = workClass.getName();
+		className = className.replace('.', '_') + "_";
+		String target = JavaStrings.toUpperCase(className);
+		workName = workName.replace(target, "");
+		int endIndex = workName.lastIndexOf('_');
+		workName = workName.substring(0, endIndex);
+		return JavaReflect.getDeclaredField(workClass, workName);
+	}
+
+	public final PrologProvider getProvider() {
+		return provider;
+	}
+
+}
---
+++ src/main/java/org/logicware/pdb/util/JavaStrings.java
@@ -0,0 +1,26 @@
+package org.logicware.pdb.util;
+
+public class JavaStrings {
+
+	public static String toUpperCase(String target) {
+		StringBuilder b = new StringBuilder();
+		for (int i = 0; i < target.length(); i++) {
+			Character c = target.charAt(i);
+			b.append(Character.toUpperCase(c));
+		}
+		return "" + b + "";
+	}
+
+	public static String toLowerCase(String target) {
+		StringBuilder b = new StringBuilder();
+		for (int i = 0; i < target.length(); i++) {
+			Character c = target.charAt(i);
+			b.append(Character.toLowerCase(c));
+		}
+		return "" + b + "";
+	}
+
+	private JavaStrings() {
+	}
+
+}
