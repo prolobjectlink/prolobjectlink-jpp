2addaf17f23623d4a99cd5ed87cffe474b21fd3e
Jose Zalacain <Jose Zalacain@AVALON>
2018-12-24 11:46:47 -0400

    Include domain test classes

---
+++ src/main/java/org/logicware/domain/geometry/Point.java
@@ -0,0 +1,109 @@
+/*
+ * #%L
+ * prolobjectlink-jpp
+ * %%
+ * Copyright (C) 2012 - 2018 Logicware Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.logicware.domain.geometry;
+
+import java.io.Serializable;
+
+public class Point implements Serializable{
+
+	private String idp;
+	private double x;
+	private double y;
+
+	public Point() {
+	}
+
+	public Point(String idp) {
+		this.idp = idp;
+	}
+
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public Point(String idp, double x, double y) {
+		this.idp = idp;
+		this.x = x;
+		this.y = y;
+	}
+
+	public String getIdp() {
+		return idp;
+	}
+
+	public void setIdp(String idp) {
+		this.idp = idp;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Point [idp=" + idp + ", x=" + x + ", y=" + y + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idp == null) ? 0 : idp.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (idp == null) {
+			if (other.idp != null)
+				return false;
+		} else if (!idp.equals(other.idp))
+			return false;
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+			return false;
+		return Double.doubleToLongBits(this.y) == Double.doubleToLongBits(other.y);
+	}
+
+}
