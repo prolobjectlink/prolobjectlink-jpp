8a3c14d05efb4221c6838850fb925c2b8a17358e
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-01-01 08:38:01 -0400

    System Try

--- src/main/java/org/logicware/web/platform/AbstractWebControl.java
+++ src/main/java/org/logicware/web/platform/AbstractWebControl.java
@@ -19,25 +19,22 @@
  */
 package org.logicware.web.platform;
 
-import java.awt.FlowLayout;
-import java.awt.HeadlessException;
-import java.awt.Rectangle;
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
+import javax.swing.JOptionPane;
 
 import org.logicware.database.DatabaseServer;
+import org.logicware.logging.LoggerUtils;
 
 public abstract class AbstractWebControl extends AbstractWebPlatform implements WebServerControl {
 
@@ -77,94 +74,67 @@ public abstract class AbstractWebControl extends AbstractWebPlatform implements 
 
 		if (!serverArgs.containsKey("nogui") && url != null) {
 
-			// Open control dialog
-			/*
-			 * Swing components should never be manipulated outside the event dispatch
-			 * thread.
-			 */
-			java.awt.EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						showServerControl(url);
-					} catch (HeadlessException e) {
-						// nop, starting from console
-					}
+			if (!SystemTray.isSupported()) {
+				LoggerUtils.info(getClass(), "SystemTray is not supported");
+				return;
+			}
+
+			SystemTray tray = SystemTray.getSystemTray();
+			Toolkit toolkit = Toolkit.getDefaultToolkit();
+			Image image = toolkit.getImage("trayIcon.png");
+
+			PopupMenu menu = new PopupMenu();
+
+			MenuItem openItem = new MenuItem("Open Explorer");
+			openItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					openBrowser(url);
 				}
 			});
-		}
-	}
+			menu.add(openItem);
 
-	private void showServerControl(final String applicationUrl) {
-
-		// Main frame
-		final String title = "Desktop Server";
-		final JFrame frame = new JFrame(title);
-		frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-
-		// Create link label and listen mouse click
-		final JLabel link = new JLabel(
-				"<html>" + "<center>Desktop Server is running at: <br>" + "<a href=\"" + applicationUrl + "\">"
-						+ applicationUrl + "</a><br>Close this window to shutdown the server.</center>" + "</html>");
-		link.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				openBrowser(applicationUrl);
-			}
-		});
-
-		// Create a panel and add components to it.
-		final JPanel contentPane = new JPanel(new FlowLayout());
-		frame.setContentPane(contentPane);
-		contentPane.add(link);
-
-		// Close confirmation
-		final JLabel question = new JLabel("This will stop the server. Are you sure?");
-		final JButton okButton = new JButton("OK");
-		final JButton cancelButton = new JButton("Cancel");
-
-		// List for close verify buttons
-		final ActionListener buttonListener = new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (e.getSource() == okButton) {
-					System.exit(0);
-				} else {
-					Rectangle bounds = frame.getBounds();
-					frame.setTitle(title);
-					contentPane.removeAll();
-					contentPane.add(link);
-					contentPane.setBounds(bounds);
-					frame.setBounds(bounds);
-					frame.setVisible(true);
-					frame.repaint();
+			MenuItem startItem = new MenuItem("Start Server");
+			startItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					openBrowser(url);
 				}
-			}
-		};
-		okButton.addActionListener(buttonListener);
-		cancelButton.addActionListener(buttonListener);
+			});
+			menu.add(startItem);
 
-		frame.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				final Rectangle bounds = frame.getBounds();
-				frame.setTitle("Confirm close");
-				contentPane.removeAll();
-				contentPane.add(question);
-				contentPane.add(okButton);
-				contentPane.add(cancelButton);
-				frame.setBounds(bounds);
-				frame.setVisible(true);
-				frame.repaint();
-			}
-		});
+			MenuItem stopItem = new MenuItem("Stop Server");
+			startItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					openBrowser(url);
+				}
+			});
+			menu.add(stopItem);
 
-		// Position the window nicely
-		final java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-		final int w = 270;
-		final int h = 95;
-		final int margin = 20;
-		frame.setBounds(new Rectangle(screenSize.width - w - margin, screenSize.height - h - margin * 2, w, h));
-		frame.toFront();
-		frame.setVisible(true);
+			MenuItem configItem = new MenuItem("Configuration");
+			startItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JOptionPane.showMessageDialog(null, "Configuration");
+				}
+			});
+			menu.add(configItem);
+
+			MenuItem closeItem = new MenuItem("Close");
+			closeItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					System.exit(0);
+				}
+			});
+			menu.add(closeItem);
+
+			TrayIcon icon = new TrayIcon(image, "Prolobjectlink Server", menu);
+			icon.setImageAutoSize(true);
+			try {
+				tray.add(icon);
+			} catch (AWTException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
 	}
 
 }
