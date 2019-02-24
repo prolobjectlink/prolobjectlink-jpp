d17e5eb814ef87d676601bac3faebda00a620417
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-02-24 19:31:46 -0400

    Remove platform server control

--- src/main/java/org/prolobjectlink/web/platform/AbstractWebControl.java
+++ src/main/java/org/prolobjectlink/web/platform/AbstractWebControl.java
@@ -32,6 +32,11 @@ import java.awt.Toolkit;
 import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -100,6 +105,92 @@ public abstract class AbstractWebControl extends AbstractWebPlatform implements 
 		stdout.println("	-x	start the prolog engine execution");
 		stdout.println("	-w	print the current work directory ");
 		stdout.println("	-f	consult a prolog file and save formatted code");
+	}
+
+	public final void openBrowser(String url) {
+
+		if (runOnLinux()) {
+
+			// See if the default browser is Konqueror by resolving the symlink.
+			boolean isDefaultKonqueror = false;
+			try {
+				// Find out the location of the x-www-browser link from path.
+				Process process = runtime.exec("which x-www-browser");
+				BufferedInputStream ins = new BufferedInputStream(process.getInputStream());
+				BufferedReader bufreader = new BufferedReader(new InputStreamReader(ins));
+				String defaultLinkPath = bufreader.readLine();
+				ins.close();
+
+				// The path is null if the link did not exist.
+				if (defaultLinkPath != null) {
+					// See if the default browser is Konqueror.
+					File file = new File(defaultLinkPath);
+					String canonical = file.getCanonicalPath();
+					if (canonical.indexOf("konqueror") != -1) {
+						isDefaultKonqueror = true;
+					}
+				}
+			} catch (IOException e1) {
+				// The symlink was probably not found, so this is ok.
+			}
+
+			// Try x-www-browser, which is symlink to the default browser,
+			// except if we found that it is Konqueror.
+			if (!started && !isDefaultKonqueror) {
+				try {
+					runtime.exec("x-www-browser " + url);
+					started = true;
+				} catch (final IOException e) {
+				}
+			}
+
+			// Try firefox
+			if (!started) {
+				try {
+					runtime.exec("firefox " + url);
+					started = true;
+				} catch (final IOException e) {
+				}
+			}
+
+			// Try mozilla
+			if (!started) {
+				try {
+					runtime.exec("mozilla " + url);
+					started = true;
+				} catch (final IOException e) {
+				}
+			}
+
+			// Try konqueror
+			if (!started) {
+				try {
+					runtime.exec("konqueror " + url);
+					started = true;
+				} catch (final IOException e) {
+				}
+			}
+
+		} else if (runOnOsX()) {
+			if (!started) {
+				try {
+					runtime.exec("open " + url);
+					started = true;
+				} catch (final IOException e) {
+				}
+			}
+		} else if (runOnWindows()) {
+
+			if (!started) {
+				try {
+					runtime.exec("cmd /c start " + url);
+					started = true;
+				} catch (final IOException e) {
+				}
+			}
+
+		}
+
 	}
 
 	public final void start() {
@@ -173,7 +264,7 @@ public abstract class AbstractWebControl extends AbstractWebPlatform implements 
 		MenuItem helpItem = new MenuItem("Help");
 		helpItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
+				JOptionPane.showMessageDialog(null, "Help");
 			}
 		});
 		menu.add(helpItem);
@@ -181,7 +272,7 @@ public abstract class AbstractWebControl extends AbstractWebPlatform implements 
 		MenuItem aboutItem = new MenuItem("About");
 		aboutItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
+				JOptionPane.showMessageDialog(null, "About");
 			}
 		});
 		menu.add(aboutItem);
